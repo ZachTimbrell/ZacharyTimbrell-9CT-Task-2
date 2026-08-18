f11640d7cf5b9a96c344c444c9a2574c9f8d3ed2
ZachTimbrell <zachary.timbrell@education.nsw.gov.au>
2026-08-18 21:13:43 +1000

    finished product
    i wrote commenst and finalised my code, i talked over with my partner and we discussed about how to make it work and this is it for main.py section

--- Main.py
+++ Main.py
@@ -1,34 +1,83 @@
-from machine import pin, ADC, PWM
+from machine import Pin, ADC, PWM
 import time
+
+heat_line = 100
+
+
+heat_detected = 0
+heat_start_time = 0
+timer_active = 0  #if not active 0 if active then 1
+total_timer = 0  #total time left on timer even after stacking
+timer_start = 0
+buzzer = 0
+auto_alarm_limit = 300
+
+#main 
+heat_sensor = ADC(Pin(26)) 
+timer_button = Pin(14, Pin.IN, Pin.PULL_DOWN)
+end_button = Pin(15, Pin.IN, Pin.PULL_DOWN)
+
+buzzer = Pin(20, Pin.OUT)
+led = PWM(Pin(16))
+
 while True:
-    heat_detected = 0
-    heat_start_time = 0
-    timer_active = 0  #if not active 0 if active then 1
-    total_timer = 0  #total time left on timer even after stacking
-    buzzer = 0
-    auto_alarm_limit = 300
+    current_time = time.time()
+    sensor_reading = heat_sensor.read()
+    timer_pressed = timer_button.value() #checks how many times it is pressed
 
-    #main program
-    heat_sensor = ADC(Pin(26)) 
-    timer_button = ADC(Pin(14))
-    end_button = ADC(Pin(16))
-
-    buzzer = Pin(20, Pin.out)
-    led = Pin(16, Pin.out)
-
-    while True:
-        current_time = time.time()
-        sensor_reading = heat_sensor.read()
-        timer_pressed = timer_button.value() #checks how many times it is pressed
-
-        if sensor_reading > heat_line:   #if the reading is more than 100 degrees it starts a timer for 3 minutes
+    if sensor_reading > heat_line:   #if the reading is more than 100 degrees it starts a timer for 3 minutes
+        if heat_detected == 0:
             heat_detected = 1
             heat_start_time = current_time
         if current_time - heat_start_time >= auto_alarm_limit:   # Once it has been more than 3 minutes the buzzer turns on
             buzzer.value(1)
-        else: 
-            buzzer(0)
-        if timer_pressed:
-          if timer_active == 0:
-               buzzer
+        
+    else: 
+       heat_detected = 0
+       heat_start_time = 0
+    
+
+    if timer_pressed:
+
+        total_timer = total_timer + 300    #Adds 5 mins to the timer
+
+        if timer_active == 0:  #If the timer is off currently
+            timer_active = 1   #Turn the timer on
+            timer_start = current_time  #Saves when the time started
+
+        time.sleep(0.3)   #small delay
+
+    if timer_active == 1:
+        tp = current_time - timer_start   #Time passed
+        time_left = total_timer - tp
+        if time_left <= 0:
+            buzzer.value(1)
+            timer_active = 0   # timer turns off as its finished
+            total_timer = 0   # resets total timer
+
+
+#led brightness
+    if timer_active == 1:
+        if time_left > 240:
+            led.duty_u16(10000)
+        elif time_left > 180:
+           led.duty_u16(20000)
+        elif time_left > 120:
+            led.duty_u16(30000)
+        elif time_left > 60:
+            led.duty_u16(45000)
+        else:
+           led.duty_u16(65535)
+
+# End BUtton
+    if end_button.value() == 1:
+        buzzer.value(0)
+        led.duty_u16(0)
+        timer_active = 0
+        total_timer = 0
+        heat_detected = 0
+        heat_start_time = 0
+        time.sleep(0.5)
+
+    time.sleep(0.01)   #so pico doesnt overheat quickly
                
